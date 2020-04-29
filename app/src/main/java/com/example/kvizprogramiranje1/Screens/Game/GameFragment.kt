08b93201cbd402278e464312f678fb3546f4117e
tarik11 <60644723+tarik11@users.noreply.github.com>
2020-04-29 15:41:00 +0200

    GameView

--- app/src/main/java/com/example/kvizprogramiranje1/Screens/Game/GameFragment.kt
+++ app/src/main/java/com/example/kvizprogramiranje1/Screens/Game/GameFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 
 import com.example.kvizprogramiranje1.R
 import com.example.kvizprogramiranje1.databinding.FragmentGameBinding
@@ -19,9 +20,14 @@ class GameFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
-
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
+        binding.NextBtn.setOnClickListener {onClik()}
+
         return binding.root
+    }
+
+    private fun onClik(){
+
     }
 }
--- app/src/main/java/com/example/kvizprogramiranje1/Screens/main/MenuFragment.kt
+++ app/src/main/java/com/example/kvizprogramiranje1/Screens/main/MenuFragment.kt
@@ -8,6 +8,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.replace
+import androidx.navigation.findNavController
 import com.example.kvizprogramiranje1.R
 import com.example.kvizprogramiranje1.databinding.FragmentMenuBinding
 
@@ -23,14 +24,17 @@ class MenuFragment : Fragment() {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_menu, container, false)
 
-        binding.easyBtn.setOnClickListener { replaceFragment() }
+        binding.easyBtn.setOnClickListener {view : View ->
+            view.findNavController().navigate(R.id.action_menuFragment_to_gameFragment2)}
+        binding.normalBtn.setOnClickListener {view : View ->
+            view.findNavController().navigate(R.id.action_menuFragment_to_gameFragment2)}
+        binding.hardBtn.setOnClickListener {view : View ->
+            view.findNavController().navigate(R.id.action_menuFragment_to_gameFragment2)}
+
 
         return binding.root
     }
 
-    private fun replaceFragment() {
-
-    }
 
 
 }
